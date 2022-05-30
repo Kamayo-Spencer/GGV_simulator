da678ecc13415190d643bfd02a79f702194e8472
Kamayo Spencer <s.kamayo@alustudent.com>
2022-05-30 18:53:12 +0300

    update

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,8 +2,9 @@ module.exports = app =>{
     const Transmitter = require("../controllers/receiverOp.js");
     var router = require('express').Router();
 
-    router.post("/add",  Transmitter.add);
+    router.post("/",  Transmitter.add);
     router.delete("/:id",Transmitter.deleteTransmitter);
+    router.get('/', Transmitter.findTransmitters)
 
     app.use('/api/tasks', router)
     
